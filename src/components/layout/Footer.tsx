44c1aced5efd9642e8c2fbb8a0e8ad680dac6410
Yevhenii <etozhunya@gmail.com>
2026-01-04 19:49:08 +0100

    fix error

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,9 +1,8 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Button } from "../ui/button";
 import { useContactModal } from "@/contexts/ContactModalContext";
 
 const Footer = () => {
-  const navigate = useNavigate();
   const { openModal } = useContactModal();
   const links = [
     { path: "/", label: "Home" },
