f268b178a8e1ba87424b75fa097f3d315ba86919
Yevhenii <etozhunya@gmail.com>
2026-01-04 19:51:07 +0100

    fix build[2]

--- src/contexts/ContactModalContext.tsx
+++ src/contexts/ContactModalContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState, ReactNode } from "react";
+import { createContext, useContext, useState, type ReactNode } from "react";
 import { Modal } from "@/components/common/Modal";
 import { ContactForm } from "@/components/ContactForm";
 
